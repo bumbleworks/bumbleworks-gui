05595c6ce5433a17905c5b1b71152ce3f361eb85
Ravi Gadad <ravi@renewfund.com>
2014-08-11 05:58:47 -0700

    Fix worker admin specs to resume worker afterwards

--- spec/features/worker_administration_spec.rb
+++ spec/features/worker_administration_spec.rb
@@ -3,39 +3,42 @@ feature "Worker management" do
   let(:worker_detail) { WorkerDetail.new(Capybara) }
 
   scenario "Admin views worker index" do
-    worker1 = Bumbleworks.start_worker!
-    worker2 = Bumbleworks.start_worker!
+    other_worker = Bumbleworks.start_worker!
 
     visit_scoped '/workers'
 
-    expect(worker_index).to have_workers([Bumbleworks.dashboard.worker, worker1, worker2])
+    expect(worker_index).to have_workers([Bumbleworks.dashboard.worker, other_worker])
+
+    other_worker.shutdown
   end
 
   scenario "Admin views worker detail" do
-    worker = Bumbleworks.start_worker!
+    visit_scoped "/workers/#{Bumbleworks.dashboard.worker.id}"
 
-    visit_scoped "/workers/#{worker.id}"
-
-    expect(worker_detail).to have_worker(worker)
+    expect(worker_detail).to have_worker(Bumbleworks.dashboard.worker)
   end
 
   scenario "Admin pauses all workers" do
-    worker1 = Bumbleworks.start_worker!
-    worker2 = Bumbleworks.start_worker!
+    other_worker = Bumbleworks.start_worker!
 
     visit_scoped "/workers"
     click_button :pause
     expect(page).not_to have_text('running')
     expect(page).to have_text('paused')
+
+    # return to single running worker
+    click_button :unpause
+    other_worker.shutdown
   end
 
   scenario "Admin purges stale worker info" do
-    worker1 = Bumbleworks.start_worker!
-    worker2 = Bumbleworks.start_worker!
+    other_worker = Bumbleworks.start_worker!
 
     visit_scoped "/workers"
     click_button :shutdown
     click_button :purge
-    expect(worker_index).not_to have_workers([worker1, worker2])
+    expect(worker_index).not_to have_workers([Bumbleworks.dashboard.worker, other_worker])
+
+    Bumbleworks.start_worker!
   end
 end
