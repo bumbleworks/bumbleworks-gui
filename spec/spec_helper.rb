ebd7cf88a7edd5710b091170211e3c62e7afb3da
Scott Duncombe <sduncombe@renewfinancial.com>
2016-07-20 16:35:45 -0700

    PAN-41: Fix tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -22,14 +22,9 @@ require_relative 'fixtures/bumbleworks_config.rb'
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
-
-  # Run specs in random order to surface order dependencies. If you find an
-  # order dependency and want to debug it, you can fix the order by providing
-  # the seed, which is printed after each run.
-  #     --seed 1234
   config.order = 'random'
 
-  config.before(:suite, :type => :features) do
+  config.before(:suite) do
     Bumbleworks.start_worker!
   end
 
@@ -37,7 +32,7 @@ RSpec.configure do |config|
     Bumbleworks.kill_all_processes!
   end
 
-  config.after(:suite, :type => :features) do
+  config.after(:suite) do
     Bumbleworks::Worker.shutdown_all
   end
 
